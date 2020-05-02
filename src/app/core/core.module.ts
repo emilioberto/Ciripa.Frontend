895f118ec00b2af224cc946c401f038e89572402
Emilio Bertò <emilio.berto1@gmail.com>
2020-05-02 16:50:38 +0200

    Localized

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,9 @@ import { LOCALE_ID, NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { formatMessage, loadMessages, locale } from 'devextreme/localization';
+import itMessages from 'devextreme/localization/messages/it.json';
+
 import { ExceptionsService } from '@app/core/services/exceptions.service';
 import { NavigationService } from '@app/core/services/navigation.service';
 import { ToastsService } from '@app/core/services/toasts.service';
@@ -31,5 +34,8 @@ export class CoreModule {
     if (parentModule) {
       throw new Error('CoreModule has already been loaded. You should only import Core modules in the AppModule only.');
     }
+
+    loadMessages(itMessages);
+    locale('it');
   }
 }
